fd82f3b5c673df8a2ba667ab9a2e926c169ee5af
imtiyaz ahmad <imtiyazah907@gmail.com>
2023-01-20 23:44:33 +0530

    added pages and compenents and tested

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Login from './Pages/Login';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <div>
       <h1> Hello React App</h1>
+      <Login></Login>
+      <Home></Home>
     </div>
   );
 }
