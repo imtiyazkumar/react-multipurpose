b22207b796fbfdf02475698111972b324c655aae
Imtiyaz <imtiyazah907@gmail.com>
2023-01-21 13:39:58 +0530

    installed Firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import './App.css';
-import Login from './Pages/Login';
-import Home from './Pages/Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header/Header';
-import NotFound from './Pages/NotFound';
 
 function App() {
   return (
-    <Router>
-      <div className='App'>
-        <h1> Hello React App</h1>
-        <Header></Header>
-        <Routes>
-          <Route path="/" element={<Home />} exact />
-          <Route path="/login" element={<Login />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className='App'>
+      <Header></Header>
+      <h1>Demo</h1>
+    </div>
   );
 }
 
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,36 @@
-import React from 'react'
-import { Link } from "react-router-dom";
+import Home from '../../Pages/Home';
+import Login from '../../Pages/Login';
+import NotFound from '../../Pages/NotFound';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
 
 function Header() {
     return (
-        <div>
-            <h1>My App</h1>
-            <nav>
-                <Link to="/">Home</Link> |{" "}
-                <Link to="login">Login</Link>
-            </nav>
-        </div>
-    )
+        <Router>
+            <div>
+                <div>
+                    <Navbar bg="light" expand="lg">
+                        <Container>
+                            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                            <Navbar.Collapse id="basic-navbar-nav">
+                                <Nav className="me-auto">
+                                    <Nav.Link as={Link} to={"/"}>Home</Nav.Link>
+                                    <Nav.Link as={Link} to={"/login"}>Login</Nav.Link>
+                                </Nav>
+                            </Navbar.Collapse>
+                        </Container>
+                    </Navbar>
+                </div>
+                <Routes>
+                    <Route path="/" element={<Home />} exact />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="*" element={<NotFound />} />
+                </Routes>
+            </div>
+        </Router >
+    );
 }
 
-export default Header
+export default Header;
